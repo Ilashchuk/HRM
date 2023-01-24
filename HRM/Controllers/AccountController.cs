cb84cce0ff516a59c76282b149d7d6b0f66d31fd
Bohdan <44bohdan44@gmail.com>
2023-01-24 19:54:30 +0200

    Some work with correct view and change home page

--- HRM/Controllers/AccountController.cs
+++ HRM/Controllers/AccountController.cs
@@ -109,7 +109,7 @@ namespace HRM.Controllers
         public async Task<IActionResult> Logout()
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return RedirectToAction("Login", "Account");
+            return RedirectToAction("Index", "Home");
         }
 
     }
